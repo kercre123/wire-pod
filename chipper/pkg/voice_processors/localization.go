273ce11b46687a8d9d0b7284a4979b65c3c0efac
frcfppud <filippo.forchino@gmail.com>
2022-11-14 08:59:14 +0100

    ProductionVectorInstructions.md

--- chipper/pkg/voice_processors/localization.go
+++ chipper/pkg/voice_processors/localization.go
@@ -3,12 +3,18 @@ package wirepod
 const STR_WEATHER_IN = "str_weather_in"
 const STR_WEATHER_FORECAST = "str_weather_forecast"
 const STR_WEATHER_TOMORROW = "str_weather_tomorrow"
+const STR_WEATHER_THE_DAY_AFTER_TOMORROW = "str_weather_the_day_after_tomorrow"
+const STR_WEATHER_TONIGHT = "str_weather_tonight"
+const STR_WEATHER_THIS_AFTERNOON = "str_weather_this_afternoon"
 
 var texts = map[string][]string{
-	//  key                 en-US   it-IT
-	STR_WEATHER_IN:       {" in ", " a "},
-	STR_WEATHER_FORECAST: {"forecast", "previsioni"},
-	STR_WEATHER_TOMORROW: {"tomorrow", "domani"},
+	//  key                 			en-US   it-IT
+	STR_WEATHER_IN:                     {" in ", " a "},
+	STR_WEATHER_FORECAST:               {"forecast", "previsioni"},
+	STR_WEATHER_TOMORROW:               {"tomorrow", "domani"},
+	STR_WEATHER_THE_DAY_AFTER_TOMORROW: {"day after tomorrow", "dopodomani"},
+	STR_WEATHER_TONIGHT:                {"tonight", "stasera"},
+	STR_WEATHER_THIS_AFTERNOON:         {"afternoon", "pomeriggio"},
 }
 
 func getText(key string) string {
