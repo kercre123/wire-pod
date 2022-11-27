4a72f160cc678c6dd59bdb0a38ac2de840bf09f7
Wire <whyyoulittle333@gmail.com>
2022-11-27 14:11:46 -0600

    Using jdocs for location and units in weather command
    Former-commit-id: f84a12bee5d470ef0b17e037d6703021c07803b9

--- chipper/pkg/voice_processors/intentparam.go
+++ chipper/pkg/voice_processors/intentparam.go
@@ -1,25 +1,17 @@
 package wirepod
 
-func paramCheckerSlots(req interface{}, intent string, slots map[string]string, isOpus bool, justThisBotNum int, botSerial string) {
-	if sttLanguage=="en-US" {
-	    paramCheckerSlotsEnUS(req, intent, slots, isOpus, justThisBotNum, botSerial)
-	} else if sttLanguage=="it-IT" {
-	    paramCheckerSlotsItIT(req, intent, slots, isOpus, justThisBotNum, botSerial)
-	}
-}
-
 func paramChecker(req interface{}, intent string, speechText string, justThisBotNum int, botSerial string) {
-	if sttLanguage=="en-US" {
-	    paramCheckerEnUS(req, intent, speechText, justThisBotNum, botSerial)
-	} else if sttLanguage=="it-IT" {
-	    paramCheckerItIT(req, intent, speechText, justThisBotNum, botSerial)
+	if sttLanguage == "en-US" {
+		paramCheckerEnUS(req, intent, speechText, justThisBotNum, botSerial)
+	} else if sttLanguage == "it-IT" {
+		paramCheckerItIT(req, intent, speechText, justThisBotNum, botSerial)
 	}
 }
 
 func prehistoricParamChecker(req interface{}, intent string, speechText string, justThisBotNum int, botSerial string) {
-	if sttLanguage=="en-US" {
-	    prehistoricParamCheckerEnUS(req, intent, speechText, justThisBotNum, botSerial)
-	} else if sttLanguage=="it-IT" {
-	    prehistoricParamCheckerItIT(req, intent, speechText, justThisBotNum, botSerial)
+	if sttLanguage == "en-US" {
+		prehistoricParamCheckerEnUS(req, intent, speechText, justThisBotNum, botSerial)
+	} else if sttLanguage == "it-IT" {
+		prehistoricParamCheckerItIT(req, intent, speechText, justThisBotNum, botSerial)
 	}
 }
