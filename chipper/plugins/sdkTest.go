4e1ea20bafb2cc0be39b95ea3f440706a051cf7d
Wire <whyyoulittle333@gmail.com>
2022-11-27 03:03:37 -0600

    Fix sdk example plugin, fix ip address replacement in botSdkInfo.json

--- chipper/plugins/sdkTest.go
+++ chipper/plugins/sdkTest.go
@@ -4,7 +4,7 @@ import (
 	"context"
 	"fmt"
 
-	"github.com/kercre123/vector-go-sdk/pkg/vector"
+	sdk_wrapper "github.com/kercre123/vector-go-sdk/pkg/sdk-wrapper"
 )
 
 // test of SDK implementation
@@ -15,15 +15,15 @@ var Name = "SDK Plugin Test"
 func Action(transcribedText string, botSerial string) string {
 	fmt.Println("hello world plugin test")
 	phrase := "hello world"
-	vector.New(
-		vector.WithTarget(""),
-		vector.WithToken(""),
-	)
+	sdk_wrapper.InitSDK(botSerial)
 	ctx := context.Background()
 	start := make(chan bool)
 	stop := make(chan bool)
 	go func() {
-		_ = sdk_wrapper.Robot.BehaviorControl(ctx, start, stop)
+		err := sdk_wrapper.Robot.BehaviorControl(ctx, start, stop)
+		if err != nil {
+			fmt.Println(err)
+		}
 	}()
 
 	for {
@@ -34,5 +34,4 @@ func Action(transcribedText string, botSerial string) string {
 			return "intent_imperative_praise"
 		}
 	}
-	return "intent_imperative_praise"
 }
