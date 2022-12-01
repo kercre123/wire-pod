5a18cf101e727206fc89eff4d484d0b14bc24fff
frcfppud <filippo.forchino@gmail.com>
2022-12-01 22:58:38 +0100

    WIP
    Former-commit-id: a13b9a7db7d4040c01edc731f51274ed8dacd53e

--- chipper/plugins/sdkTest.go
+++ chipper/plugins/sdkTest.go
@@ -4,7 +4,7 @@ import (
 	"context"
 	"fmt"
 
-	sdk_wrapper "github.com/kercre123/vector-go-sdk/pkg/sdk-wrapper"
+	sdk_wrapper "github.com/fforchino/vector-go-sdk/pkg/sdk-wrapper"
 )
 
 // test of SDK implementation
@@ -15,7 +15,7 @@ var Name = "SDK Plugin Test"
 func Action(transcribedText string, botSerial string) string {
 	fmt.Println("hello world plugin test")
 	phrase := "hello world"
-	sdk_wrapper.InitSDK(botSerial)
+	sdk_wrapper.InitSDKForWirepod(botSerial)
 	ctx := context.Background()
 	start := make(chan bool)
 	stop := make(chan bool)
