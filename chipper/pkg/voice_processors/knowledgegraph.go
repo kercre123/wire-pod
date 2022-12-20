1018b2f5d2b4130bb51d1f0421c736ef66eb703c
Wire <whyyoulittle333@gmail.com>
2022-12-19 21:43:50 -0600

    Implemented OpenAI for knowledge graph, setup.sh now reads source.sh so you don't have to re-enter API keys, implemented the intent-graph feature (if bot doesn't understand something, request gets sent to knowledge graph handler instead)

--- chipper/pkg/voice_processors/knowledgegraph.go
+++ chipper/pkg/voice_processors/knowledgegraph.go
@@ -3,6 +3,9 @@ package wirepod
 import (
 	"bytes"
 	"encoding/json"
+	"fmt"
+	"io"
+	"net/http"
 	"os"
 	"strconv"
 	"strings"
@@ -34,28 +37,56 @@ func ParseSpokenResponse(serverResponseJSON string) (string, error) {
 }
 
 func InitHoundify() {
-	if os.Getenv("HOUNDIFY_CLIENT_ID") == "" {
-		logger("Houndify Client ID not provided.")
-		HoundEnable = false
-	}
-	if os.Getenv("HOUNDIFY_CLIENT_KEY") == "" {
-		logger("Houndify Client Key not provided.")
-		HoundEnable = false
-	}
-	if HoundEnable {
-		HKGclient = houndify.Client{
-			ClientID:  os.Getenv("HOUNDIFY_CLIENT_ID"),
-			ClientKey: os.Getenv("HOUNDIFY_CLIENT_KEY"),
+	if os.Getenv("KNOWLEDGE_ENABLED") == "" {
+		// initing with old source.sh
+		if os.Getenv("HOUNDIFY_CLIENT_ID") == "" {
+			logger("Houndify Client ID not found.")
+			HoundEnable = false
+			return
 		}
-		HKGclient.EnableConversationState()
-		logger("Houndify for knowledge graph initialized!")
+		if os.Getenv("HOUNDIFY_CLIENT_KEY") == "" {
+			logger("Houndify Client Key not found.")
+			HoundEnable = false
+			return
+		}
+		if HoundEnable {
+			HKGclient = houndify.Client{
+				ClientID:  os.Getenv("HOUNDIFY_CLIENT_ID"),
+				ClientKey: os.Getenv("HOUNDIFY_CLIENT_KEY"),
+			}
+			HKGclient.EnableConversationState()
+			logger("Houndify for knowledge graph initialized!")
+		}
+	} else {
+		if os.Getenv("KNOWLEDGE_PROVIDER") == "houndify" {
+			if os.Getenv("KNOWLEDGE_ID") == "" {
+				logger("Houndify Client ID not found.")
+				HoundEnable = false
+				return
+			}
+			if os.Getenv("KNOWLEDGE_KEY") == "" {
+				logger("Houndify Client Key not found.")
+				HoundEnable = false
+				return
+			}
+			if HoundEnable {
+				HKGclient = houndify.Client{
+					ClientID:  os.Getenv("KNOWLEDGE_ID"),
+					ClientKey: os.Getenv("KNOWLEDGE_KEY"),
+				}
+				HKGclient.EnableConversationState()
+				logger("Houndify for knowledge graph initialized!")
+			}
+		} else {
+			logger("Knowledge provider: " + os.Getenv("KNOWLEDGE_PROVIDER"))
+		}
 	}
 }
 
 var NoResult string = "NoResultCommand"
 var NoResultSpoken string
 
-func kgRequestHandler(req SpeechRequest) (string, error) {
+func kgHoundifyRequestHandler(req SpeechRequest) (string, error) {
 	var transcribedText string
 	if HoundEnable {
 		logger("Sending requst to Houndify...")
@@ -123,13 +154,82 @@ func kgVADHandler(req SpeechRequest) (SpeechRequest, error) {
 	return req, nil
 }
 
+func houndifyKG(req SpeechRequest) (string, error) {
+	req, _ = kgVADHandler(req)
+	apiResponse, err := kgHoundifyRequestHandler(req)
+	return apiResponse, err
+}
+
+func openaiRequest(transcribedText string) (string, error) {
+	sendString := "You are a helpful robot called the Anki Vector. You will be given a question asked by a user and you must provide the best answer you can. It may not be punctuated or spelled correctly. Keep the answer concise yet informative. Here is the question: " + "\\" + "\"" + transcribedText + "\\" + "\""
+	logger("Making request to OpenAI...")
+	url := "https://api.openai.com/v1/completions"
+	formData := `{
+"model": "text-davinci-003",
+"prompt": "` + sendString + `",
+"temperature": 1,
+"max_tokens": 256,
+"top_p": 1,
+"frequency_penalty": 0.2,
+"presence_penalty": 0
+}`
+	req, _ := http.NewRequest("POST", url, bytes.NewBuffer([]byte(formData)))
+	req.Header.Set("Content-Type", "application/json")
+	req.Header.Set("Authorization", "Bearer "+os.Getenv("KNOWLEDGE_KEY"))
+	client := &http.Client{}
+	resp, err := client.Do(req)
+	if err != nil {
+		return "", nil
+	}
+	defer resp.Body.Close()
+	body, _ := io.ReadAll(resp.Body)
+	type openAIStruct struct {
+		ID      string `json:"id"`
+		Object  string `json:"object"`
+		Created int    `json:"created"`
+		Model   string `json:"model"`
+		Choices []struct {
+			Text         string      `json:"text"`
+			Index        int         `json:"index"`
+			Logprobs     interface{} `json:"logprobs"`
+			FinishReason string      `json:"finish_reason"`
+		} `json:"choices"`
+		Usage struct {
+			PromptTokens     int `json:"prompt_tokens"`
+			CompletionTokens int `json:"completion_tokens"`
+			TotalTokens      int `json:"total_tokens"`
+		} `json:"usage"`
+	}
+	var openAIResponse openAIStruct
+	err = json.Unmarshal(body, &openAIResponse)
+	if err != nil || len(openAIResponse.Choices) == 0 {
+		fmt.Println("ERROR: " + string(body))
+		fmt.Println("")
+		return "", err
+	}
+	apiResponse := strings.TrimSpace(openAIResponse.Choices[0].Text)
+	logger("OpenAI response: " + apiResponse)
+	return apiResponse, nil
+}
+
+func openaiKG(speechReq SpeechRequest) (string, error) {
+	transcribedText, err := sttHandler(speechReq)
+	if err != nil {
+		return "", nil
+	}
+	return openaiRequest(transcribedText)
+}
+
 func (s *Server) ProcessKnowledgeGraph(req *vtt.KnowledgeGraphRequest) (*vtt.KnowledgeGraphResponse, error) {
 	botNum = botNum + 1
-	speechReq := reqToSpeechRequest(req)
-	speechReq.BotNum = botNum
+	var apiResponse string
 	var err error
-	speechReq, err = kgVADHandler(speechReq)
-	apiResponse, _ := kgRequestHandler(speechReq)
+	speechReq := reqToSpeechRequest(req)
+	if os.Getenv("KNOWLEDGE_PROVIDER") == "houndify" || os.Getenv("HOUNDIFY_CLIENT_ID") != "" {
+		apiResponse, err = houndifyKG(speechReq)
+	} else if os.Getenv("KNOWLEDGE_PROVIDER") == "openai" {
+		apiResponse, err = openaiKG(speechReq)
+	}
 	if err != nil {
 		logger(err)
 		NoResultSpoken = err.Error()
