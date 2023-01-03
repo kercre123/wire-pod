0fce9ba044d65cd6b2157aab687b7af0bdd1ae81
Wire <whyyoulittle333@gmail.com>
2023-01-02 18:11:42 -0600

    Fix words2num, handle phrases containing 'hundred' and fix minutes

--- chipper/pkg/wirepod/ttr/words2num.go
+++ chipper/pkg/wirepod/ttr/words2num.go
@@ -10,30 +10,33 @@ import (
 var number int = 0
 
 func basicspeechText2num(speechText string) int {
-	if strings.Contains(speechText, "one") {
+	if strings.Contains(speechText, "one") && !strings.Contains(speechText, "one hundred") {
 		return 1
-	} else if strings.Contains(speechText, "two") {
+	} else if strings.Contains(speechText, "two") && !strings.Contains(speechText, "two hundred") {
 		return 2
-	} else if strings.Contains(speechText, "three") {
+	} else if strings.Contains(speechText, "three") && !strings.Contains(speechText, "three hundred") {
 		return 3
-	} else if strings.Contains(speechText, "four") {
+	} else if strings.Contains(speechText, "four") && !strings.Contains(speechText, "four hundred") {
 		return 4
-	} else if strings.Contains(speechText, "five") {
+	} else if strings.Contains(speechText, "five") && !strings.Contains(speechText, "five hundred") {
 		return 5
-	} else if strings.Contains(speechText, "six") {
+	} else if strings.Contains(speechText, "six ") && !strings.Contains(speechText, "six hundred") {
 		return 6
-	} else if strings.Contains(speechText, "seven") {
+	} else if strings.Contains(speechText, "seven ") && !strings.Contains(speechText, "seven hundred") {
 		return 7
-	} else if strings.Contains(speechText, "eight") {
+	} else if strings.Contains(speechText, "eight ") && !strings.Contains(speechText, "eight hundred") {
 		return 8
-	} else if strings.Contains(speechText, "nine") {
+	} else if strings.Contains(speechText, "nine ") && !strings.Contains(speechText, "nine hundred") {
 		return 9
 	}
 	return 0
 }
 
 func words2num(speechText string) string {
-	number = 1
+	number = basicspeechText2num(speechText)
+	if number == 0 {
+		number = 1
+	}
 	if strings.Contains(speechText, "teen") {
 		number = 10
 		if strings.Contains(speechText, "thir") {
@@ -74,6 +77,9 @@ func words2num(speechText string) string {
 		number = 80 + basicspeechText2num(speechText)
 	} else if strings.Contains(speechText, "ninety") {
 		number = 90 + basicspeechText2num(speechText)
+	}
+	if strings.Contains(speechText, "hundred") {
+		number = number + 100
 	}
 	if strings.Contains(speechText, "minute") {
 		number = number * 60
