8165a143a6895351c345daab74e86cc5f90310a2
Hamlet3000 <75612866+Hamlet3000@users.noreply.github.com>
2023-01-26 09:38:56 +0100

    colors in variables for easy change of themes

---
+++ chipper/webroot/js/faces.js
@@ -0,0 +1,39 @@
+var client = new HttpClient();
+
+function refreshFaceList() {
+var x = document.getElementById("faceList");
+x.innerHTML = ""
+fetch("/api-sdk/get_faces")
+.then(response => response.text())
+.then ((response) => {
+  if (response.includes("null")) {
+    console.log("no faces exist.")
+  } else {
+  jsonResp = JSON.parse(response)
+  for (var i = 0; i < jsonResp.length; i++){
+    var option = document.createElement("option");
+    option.text = jsonResp[i]["name"]
+    option.value = jsonResp[i]["face_id"] + ":" + jsonResp[i]["name"]
+    x.add(option);
+  }
+}
+})
+}
+
+refreshFaceList()
+
+function renameFace() {
+  var x = document.getElementById("faceList");
+  oldFaceName = x.value.split(":")[1]
+  faceId = x.value.split(":")[0]
+  newFaceName = window.prompt('Enter the new name here:');
+  fetch("/api-sdk/rename_face?oldname=" + oldFaceName + "&id=" + faceId + "&newname=" + newFaceName)
+    .then (function(){alert("Success!"); refreshFaceList()})
+}
+
+function deleteFace() {
+  var x = document.getElementById("faceList");
+  faceId = x.value.split(":")[0]
+  fetch("/api-sdk/delete_face?id=" + faceId)
+    .then (function(){alert("Success!"); refreshFaceList()})
+}
