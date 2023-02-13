600d94e0e537d3bf0f6f6c3dac8026be4d10e4a6
Wire <whyyoulittle333@gmail.com>
2023-02-12 19:44:02 -0600

    Finish initial setup page, it is redirected to from index if pod is not set up. Setup.sh now only sets up STT and downloads packages. Make chipper server restartable and ports closable during runtime

---
+++ chipper/pkg/initwirepod/web.go
@@ -0,0 +1,54 @@
+package initwirepod
+
+import (
+	"fmt"
+	"net/http"
+	"strconv"
+
+	"github.com/kercre123/chipper/pkg/vars"
+	botsetup "github.com/kercre123/chipper/pkg/wirepod/setup"
+)
+
+// cant be part of config-ws, otherwise import cycle
+
+func ChipperHTTPApi(w http.ResponseWriter, r *http.Request) {
+	switch {
+	case r.URL.Path == "/api-chipper/restart":
+		RestartServer()
+		fmt.Fprint(w, "done")
+		return
+	case r.URL.Path == "/api-chipper/use_ip":
+		port := r.FormValue("port")
+		if port == "" {
+			fmt.Fprint(w, "error: must have port")
+			return
+		}
+		_, err := strconv.Atoi(port)
+		if err != nil {
+			fmt.Fprint(w, "error: port is invalid")
+			return
+		}
+		err = botsetup.CreateCertCombo()
+		botsetup.CreateServerConfig()
+		if err != nil {
+			fmt.Fprint(w, "error: "+err.Error())
+			return
+		}
+		vars.APIConfig.Server.EPConfig = false
+		vars.APIConfig.Server.Port = port
+		vars.APIConfig.PastInitialSetup = true
+		vars.WriteConfigToDisk()
+		RestartServer()
+		fmt.Fprint(w, "done")
+		return
+	case r.URL.Path == "/api-chipper/use_ep":
+		vars.APIConfig.Server.EPConfig = true
+		vars.APIConfig.Server.Port = "443"
+		vars.APIConfig.PastInitialSetup = true
+		botsetup.CreateServerConfig()
+		vars.WriteConfigToDisk()
+		RestartServer()
+		fmt.Fprint(w, "done")
+		return
+	}
+}
