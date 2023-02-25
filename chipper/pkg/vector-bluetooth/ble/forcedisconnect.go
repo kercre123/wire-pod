61e13cabfe13b2fa0a77c984b41cb5a1611180ed
Wire <whyyoulittle333@gmail.com>
2023-02-24 23:29:36 -0600

    Fix panic condition in connection timer, implement basic OTA-via-BLE
    Former-commit-id: 03b89fd9b93ee7af787d39c125c8636e0362579c

--- chipper/pkg/vector-bluetooth/ble/forcedisconnect.go
+++
@@ -1,8 +0,0 @@
-package ble
-
-func handleRTSForceDisconnect(v *VectorBLE, msg interface{}) (data []byte, cont bool, err error) {
-	v.state.setAuth(false)
-	v.state.setClientGUID("")
-	v.state.setNonce(nil)
-	return nil, false, nil
-}
