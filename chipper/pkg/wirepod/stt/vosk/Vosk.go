67a41b49ae89de3dbbf50966f308908181db1d2f
Wire <whyyoulittle333@gmail.com>
2023-11-03 19:43:49 -0500

    Vosk processor now creates a grammer array with the intents list
    Former-commit-id: 67173f0b62cf61562e4027d738648a9144228b3c

--- chipper/pkg/wirepod/stt/vosk/Vosk.go
+++ chipper/pkg/wirepod/stt/vosk/Vosk.go
@@ -2,8 +2,10 @@ package wirepod_vosk
 
 import (
 	"encoding/json"
+	"fmt"
 	"log"
 	"strconv"
+	"time"
 
 	vosk "github.com/alphacep/vosk-api/go"
 	"github.com/kercre123/chipper/pkg/logger"
@@ -14,10 +16,46 @@ import (
 var Name string = "vosk"
 
 var model *vosk.VoskModel
+var rec *vosk.VoskRecognizer
+var mainRecInUse bool
 var modelLoaded bool
+var Grammer string
+
+func removeDuplicates(slice []string) []string {
+	seen := make(map[string]bool)
+	result := []string{}
+	for _, val := range slice {
+		if _, ok := seen[val]; !ok {
+			seen[val] = true
+			result = append(result, val)
+		}
+	}
+	return result
+}
+
+func GetGrammerList(lang string) string {
+	var wordsList []string
+	var grammer string
+	for _, words := range vars.MatchListList {
+		for _, word := range words {
+			wordsList = append(wordsList, word)
+		}
+	}
+	wordsList = removeDuplicates(wordsList)
+	for i, word := range wordsList {
+		if i == len(wordsList)-1 {
+			grammer = grammer + `"` + word + `"`
+		} else {
+			grammer = grammer + `"` + word + `"` + ", "
+		}
+	}
+	grammer = "[" + grammer + "]"
+	return grammer
+}
 
 func Init() error {
 	if vars.APIConfig.PastInitialSetup {
+		Grammer = GetGrammerList(vars.APIConfig.STT.Language)
 		vosk.SetLogLevel(-1)
 		if modelLoaded {
 			logger.Println("A model was already loaded, freeing")
@@ -36,29 +74,53 @@ func Init() error {
 			return err
 		}
 		model = aModel
+		aRecognizer, err := vosk.NewRecognizerGrm(aModel, 16000.0, Grammer)
+		//aRecognizer, err := vosk.NewRecognizer(aModel, 16000.0)
+		if err != nil {
+			log.Fatal(err)
+			return err
+		}
+		rec = aRecognizer
 		modelLoaded = true
 		logger.Println("VOSK initiated successfully")
 	}
 	return nil
 }
 
+func setRecFalse() {
+	mainRecInUse = false
+}
+
 func STT(req sr.SpeechRequest) (string, error) {
 	logger.Println("(Bot " + strconv.Itoa(req.BotNum) + ", Vosk) Processing...")
 	speechIsDone := false
-	sampleRate := 16000.0
-	rec, err := vosk.NewRecognizer(model, sampleRate)
-	if err != nil {
-		log.Fatal(err)
+	var thisRec *vosk.VoskRecognizer
+	if mainRecInUse {
+		newRec, err := vosk.NewRecognizer(model, 16000.0)
+		if err != nil {
+			log.Fatal(err)
+		}
+		thisRec = newRec
+	} else {
+		mainRecInUse = true
+		defer setRecFalse()
+		fmt.Println("using main")
+		thisRec = rec
 	}
-	rec.SetWords(1)
-	rec.AcceptWaveform(req.FirstReq)
+	//sampleRate := 16000.0
+	// rec, err := vosk.NewRecognizerGrm(model, sampleRate, Grammer)
+	// if err != nil {
+	// 	log.Fatal(err)
+	// }
+	//thisRec.SetWords(2)
+	bTime := time.Now()
+	thisRec.AcceptWaveform(req.FirstReq)
 	for {
-		var chunk []byte
-		chunk, err = req.GetNextStreamChunk()
+		chunk, err := req.GetNextStreamChunk()
 		if err != nil {
 			return "", err
 		}
-		rec.AcceptWaveform(chunk)
+		thisRec.AcceptWaveform(chunk)
 		// has to be split into 320 []byte chunks for VAD
 		speechIsDone = req.DetectEndOfSpeech()
 		if speechIsDone {
@@ -66,7 +128,8 @@ func STT(req sr.SpeechRequest) (string, error) {
 		}
 	}
 	var jres map[string]interface{}
-	json.Unmarshal([]byte(rec.FinalResult()), &jres)
+	json.Unmarshal([]byte(thisRec.FinalResult()), &jres)
+	fmt.Println("Process took: ", time.Now().Sub(bTime))
 	transcribedText := jres["text"].(string)
 	logger.Println("Bot " + strconv.Itoa(req.BotNum) + " Transcribed text: " + transcribedText)
 	return transcribedText, nil
