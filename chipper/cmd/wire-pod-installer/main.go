d212e086286c29131ff4cc75cfbd1d58683a436f
Wire <whyyoulittle333@gmail.com>
2023-11-20 22:22:16 -0600

    windows: set download URL to github
    Former-commit-id: 31bbcb1878396a7629f902635863f8e88d4a606f

--- chipper/cmd/wire-pod-installer/main.go
+++ chipper/cmd/wire-pod-installer/main.go
@@ -27,9 +27,9 @@ import (
 //go:embed ico
 var iconData embed.FS
 
-//var amd64podURL string = "https://github.com/kercre123/wire-pod/releases/latest/download/wire-pod-win-amd64.zip"
+var amd64podURL string = "https://github.com/kercre123/wire-pod/releases/latest/download/wire-pod-win-amd64.zip"
 
-var amd64podURL string = "http://192.168.1.2:82/wire-pod-win-amd64.zip"
+//var amd64podURL string = "http://192.168.1.2:82/wire-pod-win-amd64.zip"
 
 var DefaultInstallationDirectory string = "C:\\Program Files\\wire-pod"
 
