928d6f2f3defdea58a7a65196f1f174480a7a82c
kercre123 <kerigancreighton@gmail.com>
2023-12-27 22:23:18 -0600

    Fix WeatherAPI on Android

--- chipper/pkg/wirepod/ttr/weather.go
+++ chipper/pkg/wirepod/ttr/weather.go
@@ -198,7 +198,7 @@ func getWeather(location string, botUnits string, hoursFromNow int) (string, str
 			var weatherAPICladMap weatherAPICladStruct
 			mapPath := ""
 			if runtime.GOOS == "android" || runtime.GOOS == "ios" {
-				mapPath = vars.AndroidPath + "/weather-map.json"
+				mapPath = vars.AndroidPath + "/static/weather-map.json"
 			} else {
 				mapPath = "./weather-map.json"
 			}
