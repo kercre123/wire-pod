7a2960ab0e16867600f40508d68fe087506468fb
kercre123 <kerigancreighton@gmail.com>
2024-04-08 21:46:12 -0500

    Allow choice of whisper model even if dir exists

--- setup.sh
+++ setup.sh
@@ -230,32 +230,32 @@ function getSTT() {
             mkdir whisper.cpp
             cd whisper.cpp
             git clone https://github.com/ggerganov/whisper.cpp.git .
-            function whichWhisperModel() {
-                availableModels="tiny, base, small, medium, large"
-                echo
-                echo "Which Whisper model would you like to use?"
-                echo "Options: $availableModels"
-                echo '(tiny is recommended)'
-                echo
-                read -p "Enter preferred model: " whispermodel
-                if [[ ! -n ${whispermodel} ]]; then
-                    echo
-                    echo "You must enter a key."
-                    whichWhisperModel
-                fi
-                if [[ ! ${whispermodel} == *"$availableModels"* ]]; then
-                    echo
-                    echo "Invalid model."
-                    whichWhisperModel
-                fi
-            }
-            whichWhisperModel
-            ./models/download-ggml-model.sh $whispermodel
-            echo "export WHISPER_MODEL=$whispermodel" >> ./chipper/source.sh
-            cd bindings/go
-            make whisper
-            cd ${origDir}
         fi
+        function whichWhisperModel() {
+            availableModels="tiny, base, small, medium, large"
+            echo
+            echo "Which Whisper model would you like to use?"
+            echo "Options: $availableModels"
+            echo '(tiny is recommended)'
+            echo
+            read -p "Enter preferred model: " whispermodel
+            if [[ ! -n ${whispermodel} ]]; then
+                echo
+                echo "You must enter a key."
+                whichWhisperModel
+            fi
+            if [[ ! ${whispermodel} == *"$availableModels"* ]]; then
+                echo
+                echo "Invalid model."
+                whichWhisperModel
+            fi
+        }
+        whichWhisperModel
+        ./models/download-ggml-model.sh $whispermodel
+        echo "export WHISPER_MODEL=$whispermodel" >> ./chipper/source.sh
+        cd bindings/go
+        make whisper
+        cd ${origDir}
     else
     echo "export STT_SERVICE=coqui" >> ./chipper/source.sh
         if [[ ! -f ./stt/completed ]]; then
