6e4b3018f5c79bfb17516ac4c249468a27fd4884
Ash Martian <ash.lee.martian@gmail.com>
2024-09-01 00:56:20 -0700

    Prepare for bot connection display

--- chipper/webroot/sdkapp/js/auth.js
+++ chipper/webroot/sdkapp/js/auth.js
@@ -1,21 +1,31 @@
 var client = new HttpClient();
 
-var x = document.getElementById("botList");
-fetch("/api-sdk/get_sdk_info")
-  .then((response) => response.text())
-  .then((response) => {
-    if (response.includes("error")) {
-      alert("Error, it is likely no bots are authenticated.");
-    } else {
-      jsonResp = JSON.parse(response);
-      for (var i = 0; i < jsonResp["robots"].length; i++) {
-        var option = document.createElement("option");
-        option.text = jsonResp["robots"][i]["esn"];
-        option.value = jsonResp["robots"][i]["esn"];
-        x.add(option);
-      }
+async function getSDKInfo() {
+  try {
+    var response = await fetch("/api-sdk/get_sdk_info");
+    if (!response.ok) {
+      return undefined;
     }
-  });
+    var data = await response.json();
+    return data;
+  } catch (error) {
+    console.error('Unable to get SDK info:', error);
+    throw error;
+  }
+}
+
+var x = document.getElementById("botList");
+
+getSDKInfo().then((jsonResp) => {
+  for (var i = 0; i < jsonResp["robots"].length; i++) {
+    var option = document.createElement("option");
+    option.text = jsonResp["robots"][i]["esn"];
+    option.value = jsonResp["robots"][i]["esn"];
+    x.add(option);
+  }
+}).catch(() => {
+  alert("Error, it's likely no bots are authenticated");
+});
 
 function connectSDK() {
   var x = document.getElementById("botList");
