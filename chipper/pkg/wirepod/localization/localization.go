998c289f5f108237a9d2574edc5a93185976f9b2
FluxVibe <76211971+FluxVibe@users.noreply.github.com>
2025-05-12 00:23:30 +0900

    Update localization.go
    add ko-KR

--- chipper/pkg/wirepod/localization/localization.go
+++ chipper/pkg/wirepod/localization/localization.go
@@ -2,7 +2,7 @@ package localization
 
 import "github.com/kercre123/wire-pod/chipper/pkg/vars"
 
-var ValidVoskModels []string = []string{"en-US", "it-IT", "es-ES", "fr-FR", "de-DE", "pt-BR", "pl-PL", "zh-CN", "tr-TR", "ru-RU", "nt-NL", "uk-UA", "vi-VN"}
+var ValidVoskModels []string = []string{"en-US", "it-IT", "es-ES", "fr-FR", "de-DE", "pt-BR", "pl-PL", "zh-CN", "tr-TR", "ru-RU", "nt-NL", "uk-UA", "vi-VN", "ko-KR"}
 
 const STR_WEATHER_IN = "str_weather_in"
 const STR_WEATHER_FORECAST = "str_weather_forecast"
