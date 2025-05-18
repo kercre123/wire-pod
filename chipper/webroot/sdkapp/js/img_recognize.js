bc740b5be5e36f3c38b4e6019ba5194f27461220
F4310sp <fubiratan.rkj@gmail.com>
2025-05-18 18:56:38 -0300

    Added Button and JS script (test)

---
+++ chipper/webroot/sdkapp/js/img_recognize.js
@@ -0,0 +1,21 @@
+function img_recognize() {
+    request = sendForm("/api-sdk/api-sdk/image_detection?");
+    request.onreadystatechange = function() {
+        if (request.readyState === 4 && request.status === 200) {
+            const response = JSON.parse(request.responseText);
+            if (response && response.base64String) {
+            const imageUrl = "data:image/png;base64," + response.base64String;
+            const newTab = window.open();
+            if (newTab) {
+                newTab.document.body.innerHTML = `<img src="${imageUrl}" alt="Recognized Image">`;
+            } else {
+                console.error("Failed to open a new tab");
+            }
+            } else {
+            console.error("Base64 string not found in response");
+            return null;
+            }
+        }
+    }
+
+}
