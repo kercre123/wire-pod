b90eda4f09705614f3774ed0602f0721562e4c3d
F4310sp <fubiratan.rkj@gmail.com>
2025-05-18 19:18:51 -0300

    Added Button and JS script (test)

--- chipper/webroot/sdkapp/js/img_recognize.js
+++ chipper/webroot/sdkapp/js/img_recognize.js
@@ -19,3 +19,15 @@ function img_recognize() {
     }
 
 }
+
+function sendForm(formURL) {
+    let xhr = new XMLHttpRequest();
+    if (formURL.includes("?")) {
+      formURL = formURL + "&serial=" + esn;
+    } else {
+      formURL = formURL + "?serial=" + esn;
+    }
+    xhr.open("POST", formURL);
+    xhr.setRequestHeader("Content-Type", "application/x-www-form-urlencoded");
+    xhr.send();
+  }
