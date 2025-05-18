f7f65dd12579d6806e6fe9b00be2e40ac285f344
F4310sp <fubiratan.rkj@gmail.com>
2025-05-18 19:35:00 -0300

    Fix variable declaration in img_recognize function

--- chipper/webroot/sdkapp/js/img_recognize.js
+++ chipper/webroot/sdkapp/js/img_recognize.js
@@ -1,5 +1,5 @@
 function img_recognize() {
-    request = sendForm("/api-sdk/image_detection");
+    let request = sendForm("/api-sdk/image_detection");
     request.onreadystatechange = function() {
         if (request.readyState === 4 && request.status === 200) {
             const response = JSON.parse(request.responseText);
