5ee7be0a9372118ceabc69baccaf153b01ceaa62
F4310sp <fubiratan.rkj@gmail.com>
2025-05-19 13:35:33 -0300

    Refactor img_recognize function to improve code readability and maintainability

--- chipper/webroot/sdkapp/js/img_recognize.js
+++ chipper/webroot/sdkapp/js/img_recognize.js
@@ -1,13 +1,17 @@
 async function img_recognize() {
+    //assume behavior control
     sendForm('/api-sdk/assume_behavior_control?priority=high')
+    //send image detection request
     try {
-    let response = await sendForm("/api-sdk/image_detection");
-    console.log("Teste: "+response);
-    sendForm("/api-sdk/say_text?text=" + response);
+        let response = await sendForm("/api-sdk/image_detection");
+        console.log("Teste: " + response);
+        sendForm("/api-sdk/say_text?text=I see " + response);
     } catch (error) {
         console.error('Error:', error);
     }
-   sendForm("/api-sdk/release_behavior_control")
+    //release behavior control
+    await new Promise(resolve => setTimeout(resolve, 2000));
+    sendForm("/api-sdk/release_behavior_control")
 }
 
 async function sendForm(formURL) {
@@ -20,7 +24,7 @@ async function sendForm(formURL) {
     let response = await fetch(formURL, {
         method: 'POST',
         headers: {
-           'Content-Type': 'application/x-www-form-urlencoded',
+            'Content-Type': 'application/x-www-form-urlencoded',
         }
     });
     if (!response.ok) {
