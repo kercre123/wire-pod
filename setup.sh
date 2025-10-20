2a3e65be0c7d801866e41c292dbad850142ec92e
Raj-jyot Singh Deol <deolrajjyot@gmail.com>
2025-10-19 22:19:56 -0400

    Wirepod needs root for the wakeword trigger functionality when running as service
    This works fine when running `sudo ./chipper/start.sh`, but not with the systemd daemon, this fixes that

--- setup.sh
+++ setup.sh
@@ -551,7 +551,8 @@ function setupSystemd() {
     echo "StartLimitIntervalSec=500" >>wire-pod.service
     echo "StartLimitBurst=5" >>wire-pod.service
     echo >>wire-pod.service
-    echo "[Service]" >>wire-pod.service
+	echo "[Service]" >>wire-pod.service
+    echo "User=root" >>wire-pod.service
     echo "Type=simple" >>wire-pod.service
     echo "Restart=on-failure" >>wire-pod.service
     echo "RestartSec=5s" >>wire-pod.service
